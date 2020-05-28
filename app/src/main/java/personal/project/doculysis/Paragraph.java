b14b324d92ae698b7149241f5a0c54eb4e8353cb
Aayush Shah <aayushshah719@gmail.com>
2020-05-28 22:09:10 +0530

    files regarding previous summary algorithm added

---
+++ app/src/main/java/personal/project/doculysis/Paragraph.java
@@ -0,0 +1,12 @@
+package personal.project.doculysis;
+import java.util.ArrayList;
+
+public class Paragraph {
+    int number;
+    ArrayList<Sentence> sentences;
+
+    Paragraph(int number) {
+        this.number = number;
+        sentences = new ArrayList<Sentence>();
+    }
+}
